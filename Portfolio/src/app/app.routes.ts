30ad71d60edeba68c0fb22c85d6aafac9a2bfd39
thedineshjoshi <9868459709dinesh@gmail.com>
2024-08-25 22:20:41 +0545

    new component timeline introduced

--- Portfolio/src/app/app.routes.ts
+++ Portfolio/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 export const routes: Routes = [
     {path:'\home',component:HomeComponent,pathMatch:'full'},
     { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {path:'about',component:AboutComponent,pathMatch:'full'},
+    {path:'timeline',component:TimelineComponent,pathMatch:'full'}
 ];
--- Portfolio/src/app/home/home.component.ts
+++ Portfolio/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { AboutComponent } from "../about/about.component";
+import { TimelineComponent } from "../timeline/timeline.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AboutComponent],
+  imports: [AboutComponent, TimelineComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
